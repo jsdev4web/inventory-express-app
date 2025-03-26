b3e6be510e2bf98204fc3f1a612addf4ebe604bc
jsweb4dev <jsweb4dev@gmail.com>
2025-03-26 14:54:49 -0400

    update

--- db/pool.js
+++ db/pool.js
@@ -5,9 +5,12 @@ const fs = require("fs")
 // We're hardcoding them here for simplicity
 
 module.exports = new Pool({
-  host: "postgresql://jsdev4web:j1QLj68sMZUHMiuRnU6FhMSY8n7nbCJe@dpg-cvhf09trie7s73enf4r0-a.oregon-postgres.render.com/jsdev4web?sslmode=disable", // or wherever the db is hosted
-  user: "jsdev4web",
-  database: "jsdev4web",
-  password: "j1QLj68sMZUHMiuRnU6FhMSY8n7nbCJe",
-  port: 5432 // The default port
+  host: "ep-bold-river-a20i7id6.eu-central-1.pg.koyeb.app", // or wherever the db is hosted
+  user: "koyeb-adm",
+  database: "koyebdb",
+  password: "npg_KoTs5cDMqxe9",
+  port: 5432, // The default port
+  ssl: {
+    rejectUnauthorized: false // Or true, depending on your certificate setup
+  }
 });
