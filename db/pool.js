0c168ee59b2650606a49f4138c6577b7617ca95a
jsweb4dev <jsweb4dev@gmail.com>
2025-03-26 14:00:16 -0400

    update

--- db/pool.js
+++ db/pool.js
@@ -5,7 +5,7 @@ const fs = require("fs")
 // We're hardcoding them here for simplicity
 
 module.exports = new Pool({
-  host: "postgresql://jsdev4web:j1QLj68sMZUHMiuRnU6FhMSY8n7nbCJe@dpg-cvhf09trie7s73enf4r0-a.oregon-postgres.render.com/jsdev4web", // or wherever the db is hosted
+  host: "postgresql://jsdev4web:j1QLj68sMZUHMiuRnU6FhMSY8n7nbCJe@dpg-cvhf09trie7s73enf4r0-a.oregon-postgres.render.com/jsdev4web?sslmode=disable", // or wherever the db is hosted
   user: "jsdev4web",
   database: "jsdev4web",
   password: "j1QLj68sMZUHMiuRnU6FhMSY8n7nbCJe",
